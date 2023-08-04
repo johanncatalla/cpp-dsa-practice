74084d4af2070a2f00da293723a731f1812eaea4
Johann Sebastian Catalla <101336933+johanncatalla@users.noreply.github.com>
2023-08-04 14:03:15 +0000

    const

--- POINTERS-REFERENCES/const.cpp
+++ POINTERS-REFERENCES/const.cpp
@@ -14,10 +14,24 @@ int main()
     cpr = &k;
 
     // However, a pointer is also an object so we can also make it a const itself.
-    // In this pointer, we cannot change the value of the object we are pointing to and 
+    // In this pointer, we can't change the value of the object we are pointing to and 
     // we cannot change the address the pointer is assigned.
     const int *const pnter = &j;
 
-    std::cout << pnter;
+    // Another example is a const pointer to a nonconst object
+    // We cannot change the address it is assigned buy we can modify the object it is pointing to
+    int *const pnter1 = &k;
+    *pnter1 = 666;
+
+    // a pointer should not be uninitialized as it can cause crashes and undefined behavior
+    // instead assign it to a valid address or use nullptr as initializer
+    int *ptr = nullptr;
+
+    const int ci = 42; // top level
+    const int *p2 = &ci; // low-level
+    const int *const p3 = p2; // right: = top-level, left = low-level
+
+
+    std::cout << p2;
     return 0;
 }
