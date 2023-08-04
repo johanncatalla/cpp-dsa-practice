1a48e9ba06ed9024f2fd38c0c9dcec6fe7fbcb31
Johann Sebastian Catalla <101336933+johanncatalla@users.noreply.github.com>
2023-08-04 14:58:57 +0000

    list

---
+++ DSA/list/basic.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <list>
+int main()
+{
+    std::list<int> l; 
+    l.push_front(1);
+    l.push_front(2);
+    l.push_front(3);
+
+    // size
+    unsigned int size = l.size();
+
+    for(std::list<int>::iterator it = l.begin(); it !=l.end(); it++) {
+        std::cout << *it << std::endl;
+    }
+    for (auto v : l)
+        std::cout << v;
+    
+    return 0;
+}
---
+++ OOP/basic.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+// Parent Class
+class Shape {
+    public:
+        void setWidth(int w) {
+            width = w;
+        }
+        void setheight(int h) {
+            height = h;
+        }
+    protected:
+        int width;
+        int height;
+};
+
+// Child Class
+class Rectangle: public Shape {
+    public:
+        int getArea() {
+            return (width * height);
+        }
+};
+
+int main(void) {
+    Rectangle Rect;
+
+    Rect.setWidth(10);
+    Rect.setheight(5);
+
+    std::cout << "Total Area: " << Rect.getArea() << std::endl;
+    
+    return 0;
+}
