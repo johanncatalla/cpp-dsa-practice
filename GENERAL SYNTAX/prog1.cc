659e7576ff77d03e60f750ef9613c5897ee587b4
johanncatalla <catallajohann@gmail.com>
2023-08-06 20:19:51 +0800

    test

--- GENERAL SYNTAX/prog1.cc
+++ GENERAL SYNTAX/prog1.cc
@@ -2,7 +2,7 @@
 
 int main() {
   int num1 = 1, num2 = 2;
-  std::cout << "The product of " << num1 << " and " << num2 << " is "
+  std::cout << "The product offf" << num1 << " and " << num2 << " is "
             << num1 * num2 << std::endl;
   return 0;
 }
