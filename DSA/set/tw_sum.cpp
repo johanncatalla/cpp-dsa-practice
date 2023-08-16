31434faaf50eb2be11e06a150740cbe0ff0a7b51
johanncatalla <catallajohann@gmail.com>
2023-08-16 18:44:15 +0800

    map

--- DSA/set/tw_sum.cpp
+++ DSA/set/tw_sum.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 class Solution{
 public:
-    std::vector<int> twoSum(std::vector<int> &nums, int target);
+    std::vector<int> twoSum(std::vector<int> &nums, int target) {
+        std::unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (mp.find(complement) == mp.end()) {
+                mp[nums[i]] = i;
+            } else {
+                return {mp[complement], i};
+            }
+        }
+        return{-1, -1};
+    }
 };
+
+// implementation
+int main() {
+    Solution sol;
+    std::vector<int> v = {2,3,11,15,6};
+    int target = 9;
+
+    std::vector<int> res = sol.twoSum(v, target);
+
+    for (auto iter = res.begin(); iter != res.end(); iter++) {
+        std::cout << *iter << " ";
+    }
+
+    return 0;
+}
