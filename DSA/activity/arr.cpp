a1a59f9698eedb666f8ad6b79b070f3b5add0b86
johanncatalla <catallajohann@gmail.com>
2023-09-17 22:42:10 +0800

    dsa

---
+++ DSA/activity/arr.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int size, x, y; 
+    cout << "Enter array size: ";
+    cin >> size;
+    cout << "Enter xy: ";
+    cin >> x;
+    cin >> y;
+    int arr[size] = {};
+
+    for (int i = 0; i < size; i++) {
+        std::cout << "Enter Element #" << i+1 << ": ";
+        cin >> arr[i];
+    }
+    for (int i = 0; i < size; i++) {
+        if (arr[i] >= x && arr[i] <= y) {
+            cout << "Element #" << i+1 << " = YES\n";
+        } else {
+            cout << "Element #" << i+1 << " = NO\n";
+        }
+    }
+}
